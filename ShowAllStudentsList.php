67298b5991dee53daf0340bdc81814704b5a9c2d
denysetiawan28 <denysetiawan28@gmail.com>
2019-11-01 09:54:42 +0700

    update
    test

--- ShowAllStudentsList.php
+++ ShowAllStudentsList.php
@@ -1,5 +1,18 @@
 <?php
-include 'DBConfig.php';
+//include 'DBConfig.php';
+//Define your host here.
+echo "test";
+
+$HostName = "remotemysql.com";
+
+//Define your database name here.
+$DatabaseName = "nGSWY8FyYM";
+
+//Define your database username here.
+$HostUser = "nGSWY8FyYM";
+
+//Define your database password here.
+$HostPass = "g2u3TwKrwN";
  
 // Create connection
 $conn = new mysqli($HostName, $HostUser, $HostPass, $DatabaseName);
